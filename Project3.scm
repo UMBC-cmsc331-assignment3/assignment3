739346e44e73c4c0266cb931a06e514fd49f33e3
T-Song <T-Song@users.noreply.github.com>
2014-10-06 22:39:58 -0400

    Update Project3.scm

--- Project3.scm
+++ Project3.scm
@@ -208,6 +208,8 @@
 (if (and (> k 0) (< k n))
 	(define k (prompt-read "The number of people cannot be negative and cannot be greater than number of students. Enter people per group: ")))
 (define iters (prompt-read "Attempts: "))
+(if (< iters 1)
+	(define iters (prompt-read "There cannot be a negative amount of attempts. Attempts: ")))
 
 
 ; todos
